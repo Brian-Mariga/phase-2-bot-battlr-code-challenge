22cd82af78e120ce65ed4da1ac698303c57ad913
Brian <brianmariga0@gmail.com>
2024-02-12 17:13:57 +0300

    Navbar complete

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,10 @@
-import { Link } from "@chakra-ui/react";
+// import { Link } from "@chakra-ui/react";
 
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div
-      className="navbar
-    "
-    >
+    <div className="navbar">
       <Link to="/">Bot Collection</Link>
       <Link to="/bot-army">Your Bot Army</Link>
     </div>
