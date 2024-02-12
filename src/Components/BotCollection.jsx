7b1bf4043f6f9ef212f6b4e60ef29bd4f414babf
Brian <brianmariga0@gmail.com>
2024-02-12 09:25:33 +0300

    Implemented Loading and Error

--- src/Components/BotCollection.jsx
+++ src/Components/BotCollection.jsx
@@ -14,50 +14,74 @@ import { useEffect, useState } from "react";
 
 function BotCollection() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8001/bots")
-      .then((r) => r.json())
-      .then((data) => setData(data), console.log(data));
-  }, [data]);
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  const cards = data.map((bot) => (
+    <Card
+      maxW="sm"
+      key={bot.id}
+      borderWidth="1px"
+      borderRadius="lg"
+      borderColor="blue"
+      overflow="hidden"
+      width="300px"
+      margin="1rem"
+      alignItems="center"
+      justifyContent="center"
+      background="#eaf6ff"
+    >
+      <CardBody>
+        <Heading size="md">Name: {bot.name}</Heading>
+        <Text color="black">ID: {bot.id}</Text>
+        <Image src={bot.avatar_url} alt={bot.name} borderRadius="lg" />
+        <Stack mt="6" spacing="3">
+          <Text color="black">Class: {bot.bot_class}</Text>
+          <Text color="green">Health: {bot.health}</Text>
+          <Text color="red">Damage: {bot.damage}</Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <ButtonGroup spacing="2">
+          <Button
+            variant="solid"
+            colorScheme="red"
+            w="200px"
+            alignItems="center"
+            justifyContent="center"
+          >
+            X
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
+  ));
 
   return (
-    <>
-      {data.map((bot) => (
-        <Card
-          maxW="sm"
-          key={bot.id}
-          borderWidth="1px"
-          borderRadius="lg"
-          borderColor="blue"
-          overflow="hidden"
-          width="300px"
-          margin="1rem"
-        >
-          <CardBody>
-            <Image src={bot.avatar_url} alt={bot.name} borderRadius="lg" />
-            <Stack mt="6" spacing="3">
-              <Heading size="md">Bot_Name: {bot.name}</Heading>
-              <Text>Bot_class: {bot.bot_class}</Text>
-              <Text color="blue.600" fontSize="2xl">
-                Bot_Health: {bot.health}
-              </Text>
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <ButtonGroup spacing="2">
-              <Button variant="solid" colorScheme="blue">
-                Buy now
-              </Button>
-              <Button variant="ghost" colorScheme="blue">
-                Add to cart
-              </Button>
-            </ButtonGroup>
-          </CardFooter>
-        </Card>
-      ))}
-    </>
+    <div>
+      {error && <p>{error}</p>}
+      {loading && <p>Loading...</p>}
+      <div>{cards}</div>
+    </div>
   );
 }
 
---
+++ src/Components/YourBotArmy.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function YourBotArmy() {
+
+    
+}
+
+export default YourBotArmy;
